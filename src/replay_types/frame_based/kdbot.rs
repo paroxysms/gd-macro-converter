300aa5ea5e9826d871b39d3840da64ab63f0ddf9
Alpha <60198552+paroxysms@users.noreply.github.com>
2022-08-03 03:21:26 -0400

    also forgot to remove comments n shit thats my bad x2

--- src/replay_types/frame_based/kdbot.rs
+++ src/replay_types/frame_based/kdbot.rs
@@ -7,21 +7,8 @@ pub struct KDBot {
 
 impl KDBot {
 
-    // function parseKDBot(view) {
-    //     const fps = view.getFloat32(0, true);
-    //     const actions = [];
-    //     for (let i = 4; i < view.byteLength; i += 6) {
-    //         const frame = view.getInt32(i, true);
-    //         const hold = view.getUint8(i + 4) === 1;
-    //         const player2 = view.getUint8(i + 5) === 1;
-    //         actions.push({x: frame, hold, player2});
-    //     }
-    //     return {fps, actions};
-    // }
-
     pub fn parse(mut file: File) -> KDBot {
         let mut buffer: [u8; 4] = [0u8; 4];
-        let mut double_buffer: [u8; 2] = [0u8; 2];
         let mut single_buffer: [u8; 1] = [0u8; 1];
 
         file.read(&mut buffer).unwrap();
@@ -47,18 +34,6 @@ impl KDBot {
             actions,
         }
     }
-
-    // function dumpKDBot(replay) {
-    //     const buffer = new ArrayBuffer(4 + replay.actions.length * 6);
-    //     const view = new DataView(buffer);
-    //     view.setFloat32(0, replay.fps, true);
-    //     replay.actions.forEach((action, i) => {
-    //         view.setUint32(4 + i * 6, action.x, true);
-    //         view.setUint8(8 + i * 6, action.hold);
-    //         view.setUint8(9 + i * 6, action.player2);
-    //     });
-    //     return buffer;
-    // }
 
     pub fn dump(actions: Vec<(i32, bool, bool)>, fps: f32, mut file: File) -> File {
         file.write_all(&(fps).to_le_bytes()).unwrap();
